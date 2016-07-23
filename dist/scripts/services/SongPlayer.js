4243af1a81c5d73f91aed2c5b1ff4cb93b8e09fb
hcbviolet <hcbviolet@gmail.com>
2016-07-23 10:25:18 -0700

    Update seekbar fill and thumb as song plays

--- dist/scripts/services/SongPlayer.js
+++ dist/scripts/services/SongPlayer.js
@@ -1,5 +1,5 @@
 (function(){
-    function SongPlayer(Fixtures) {
+    function SongPlayer($rootScope, Fixtures) {
         var SongPlayer = {};
         /**
         @desc: current album data from fixtures service
@@ -24,6 +24,12 @@
         SongPlayer.currentSong = null;
         
         /**
+        * @desc Current playback time (in seconds) of currently playing song
+        * @type {Number}
+        */
+        SongPlayer.currentTime = null;
+        
+        /**
         @desc: Buzz object audio file
         @type: {Object}
         **/
@@ -42,6 +48,12 @@
             currentBuzzObject = new buzz.sound(song.audioUrl, {
                 formats: ['mp3'],
                 preload: true
+            });
+            
+            currentBuzzObject.bind('timeupdate', function() {
+                $rootScope.$apply(function() {
+                    SongPlayer.currentTime = currentBuzzObject.getTime();
+                });
             });
             
             SongPlayer.currentSong = song;
@@ -136,7 +148,18 @@
         
     };
     
+    /**
+    * @function setCurrentTime
+    * @desc Set current time (in seconds) of currently playing song
+    * @param {Number} time
+    */
+    SongPlayer.setCurrentTime = function(time) {
+     if (currentBuzzObject) {
+         currentBuzzObject.setTime(time);
+     }
+    };
+    
     angular
         .module('blocJams')
-        .factory('SongPlayer',SongPlayer);
+        .factory('SongPlayer',['$rootScope', 'Fixtures', SongPlayer]);
 })();
